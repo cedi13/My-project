b4543d49da328f21ebab75faa226b57a72346c99
John Cedric Junas <johncedricjunas@gmail.com>
2025-02-25 00:53:00 +0800

    Feat:seeding data into database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,10 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
+
+
+        
+
         $this->call([
             CategoriesTableSeeder::class,
             ItemsTableSeeder::class,        ]);
