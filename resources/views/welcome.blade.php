b500328e738226cf2b011c69142c73499090c283
John Cedric Junas <johncedricjunas@gmail.com>
2025-02-25 01:04:23 +0800

    Feat:Making page responsive and Integrating Bootstrap

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -155,7 +155,7 @@
                                     </svg>
                                 </div>
 
-                                
+
 
                                 <div class="pt-3 sm:pt-5">
                                     <h2 class="text-xl font-semibold text-black dark:text-white">Vibrant Ecosystem</h2>
@@ -167,6 +167,9 @@
                             </div>
                         </div>
                     </main>
+
+
+                    
 
                     <footer class="py-16 text-center text-sm text-black dark:text-white/70">
                         Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
